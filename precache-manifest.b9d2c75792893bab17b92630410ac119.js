73fac6504b94a04ef7222af19a7a7e1be1c93336
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-09 09:45:55 +0530

    Updates

---
+++ precache-manifest.b9d2c75792893bab17b92630410ac119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216d0f97a235d5d53467a2540a6ed8ec",
+    "url": "/CovidTrackerApp/index.html"
+  },
+  {
+    "revision": "e461ab98f1071c4a23eb",
+    "url": "/CovidTrackerApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "040a222fbc30753e76e7",
+    "url": "/CovidTrackerApp/static/css/main.577a8f5c.chunk.css"
+  },
+  {
+    "revision": "e461ab98f1071c4a23eb",
+    "url": "/CovidTrackerApp/static/js/2.28ee6076.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "/CovidTrackerApp/static/js/2.28ee6076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040a222fbc30753e76e7",
+    "url": "/CovidTrackerApp/static/js/main.3d1c1074.chunk.js"
+  },
+  {
+    "revision": "2669740dda66207a168a",
+    "url": "/CovidTrackerApp/static/js/runtime-main.63b93fd4.js"
+  }
+]);
