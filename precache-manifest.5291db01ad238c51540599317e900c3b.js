9404e783c21321711ffa1cbe7f9279bac009ed8d
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-07-09 13:27:23 +0530

    Updates

--- precache-manifest.5291db01ad238c51540599317e900c3b.js
+++ precache-manifest.5291db01ad238c51540599317e900c3b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd9c1c7a7c6871141ab252240c295066",
+    "revision": "a8e5f00dc1d1f5a233cbeb74331d3798",
     "url": "/CovidTrackerApp/index.html"
   },
   {
-    "revision": "e461ab98f1071c4a23eb",
+    "revision": "6f4c7ecd2f91351ecfb9",
     "url": "/CovidTrackerApp/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "f0a1aca3921ccc4bf9df",
+    "revision": "3b4be4cfd203812f7240",
     "url": "/CovidTrackerApp/static/css/main.577a8f5c.chunk.css"
   },
   {
-    "revision": "e461ab98f1071c4a23eb",
-    "url": "/CovidTrackerApp/static/js/2.28ee6076.chunk.js"
+    "revision": "6f4c7ecd2f91351ecfb9",
+    "url": "/CovidTrackerApp/static/js/2.84938d37.chunk.js"
   },
   {
     "revision": "5ad387cd57eb31f6f50768be47fe3b09",
-    "url": "/CovidTrackerApp/static/js/2.28ee6076.chunk.js.LICENSE.txt"
+    "url": "/CovidTrackerApp/static/js/2.84938d37.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0a1aca3921ccc4bf9df",
-    "url": "/CovidTrackerApp/static/js/main.34fb59a7.chunk.js"
+    "revision": "3b4be4cfd203812f7240",
+    "url": "/CovidTrackerApp/static/js/main.1b1f364c.chunk.js"
   },
   {
     "revision": "2669740dda66207a168a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CovidTrackerApp/precache-manifest.ba7321a93e617993c4b1d8490ecb7945.js"
+  "/CovidTrackerApp/precache-manifest.5291db01ad238c51540599317e900c3b.js"
 );
 
 self.addEventListener('message', (event) => {
